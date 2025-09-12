2041937c3487281d890ae7c5215e080cfcc26e8f
Peter Heller <52469803+Frozenbitz@users.noreply.github.com>
2025-09-12 18:57:52 +0200

    added some tests and updated main ref

--- tests/test_parse_opcua_binary_diagnostic.py
+++ tests/test_parse_opcua_binary_diagnostic.py
@@ -1,10 +1,12 @@
 from scapy.all import *
 from scapy.contrib.opcua_binary import (
+    AdditionalHeader,
     OPC_UA_Binary,
     OPC_UA_Binary_Hello,
     OPC_UA_Binary_Ack,
     OPC_UA_Binary_SecureConversationMessage,
     OPC_UA_Binary_Error,
+    RequestHeader,
 )
 
 
@@ -30,8 +32,21 @@ def test_opcua_binary_diagnostic_messages():
     del pkt
 
 
-def main():
+def test_opcua_binary_diag_opensecurechannel_request():
+    OPN_SEC_CHANNEL_requestHeader = bytes.fromhex(
+        "0000a85e2235e965d8010000000000000000ffffffff00000000000000"
+    )
 
+    pkt = RequestHeader(OPN_SEC_CHANNEL_requestHeader)
+    assert pkt[RequestHeader].NodeID == 0
+    assert pkt[RequestHeader].RequestHandle == 0
+    assert pkt[RequestHeader].ReturnDiagnostics == 0
+    assert pkt[RequestHeader].TimeoutHint == 0
+    assert pkt[RequestHeader].AH_NodeID == 0
+    assert pkt[RequestHeader].EncodingMask == 0
+
+
+def main():
     test_opcua_binary_diagnostic_messages()
     print("all tests ran successfuly")
 
